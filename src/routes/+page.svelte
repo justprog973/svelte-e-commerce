fc52c84f5aa510cd5e0781598ba7e4b12e1b6845
justprog973 <edge973@hotmail.fr>
2024-03-06 23:41:50 -0300

    lopping the products

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,11 @@
 </script>
 
 <p class="text-white">
-	{JSON.stringify(data)}
+	{#if data.products.length > 0}
+		{#each data.products as product}
+			<p>{product.name} / {product.price} / {product.description}</p>
+		{/each}
+	{:else}
+		<p class="w-full text-center pt-32">No products are currently available</p>
+	{/if}
 </p>

a9e52acdfed23eaa85d20ccd1f74a3913864b80b
justprog973 <edge973@hotmail.fr>
2024-03-06 22:39:15 -0300

    Creating error custom page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,4 @@
+<div class="h-screen gap-3 flex justify-center items-center flex-col">
+	<h1 class="text-red-500 text-5xl text-center">Ouuups This is a custom error page</h1>
+	<p>You are try to access non-existing data, dummy!</p>
+</div>

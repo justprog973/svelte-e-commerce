c2b5b0d3680725deb852201b6d93d96033c73a09
justprog973 <edge973@hotmail.fr>
2024-03-06 23:11:32 -0300

    Creating layout page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
-	import "../app.css";
+	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<slot />
+<main class="bg-[#171717] min-h-screen flex flex-col justify-between px-5 md:px-50">
+	<Header />
+		<div class="flex-1">
+			<slot />
+		</div>
+	<Footer />
+</main>
